72315e1bc51547b2177003ad139605ff9a545ef4
ChristianKraemerBeegy <christian.kraemer@beegy.com>
2021-08-17 17:33:40 +0200

    Fixed async

--- index.js
+++ index.js
@@ -3,24 +3,27 @@ const github = require('@actions/github');
 const http = require('@actions/http-client');
 const httpAuth = require('@actions/http-client/auth');
 
-try {
-  const githubToken = core.getInput('github-token');
-  const maxArtifacts = core.getInput('max-artifacts');
 
-  const listArtifactsUrl = 'https://api.github.com/repos/beegy-dev/shaun/actions/artifacts';
+async function run() {
+  try {
+    const githubToken = core.getInput('github-token');
+    const maxArtifacts = core.getInput('max-artifacts');
+
+    const listArtifactsUrl = 'https://api.github.com/repos/beegy-dev/shaun/actions/artifacts';
         
-  client = new http.HttpClient('action/artifact', [
-	new httpAuth.BearerCredentialHandler(githubToken)
-  ]);
+    client = new http.HttpClient('action/artifact', [
+	  new httpAuth.BearerCredentialHandler(githubToken)
+    ]);
 
-  requestOptions = {
-  	'Accept': 'application/vnd.github.v3+json'
-  };
+    requestOptions = {
+  	  'Accept': 'application/vnd.github.v3+json'
+    };
 
-  const response = await client.get(listArtifactsUrl, requestOptions);
-  const body = await response.readBody();
-  console.log(body);
+    const response = await client.get(listArtifactsUrl, requestOptions);
+    const body = await response.readBody();
+    console.log(body);
 
-} catch (error) {
-  core.setFailed(error.message);
+  } catch (error) {
+    core.setFailed(error.message);
+  }
 }
